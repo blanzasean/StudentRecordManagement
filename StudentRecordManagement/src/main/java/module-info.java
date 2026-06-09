336ea8b239373cdadd984a464849c6d57f75a484
blanzasean <blanzasean@gmail.com>
2026-06-09 12:04:27 +0800

    Delete StudentRecordManagement directory

--- StudentRecordManagement/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.studentrecordmanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens org.example.studentrecordmanagement to javafx.fxml;
-    exports org.example.studentrecordmanagement;
-}
